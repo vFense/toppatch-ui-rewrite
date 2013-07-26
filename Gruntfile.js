393cc533ee59498a33640b59728fba0b232f63a5
Luis Aleman <Luis@toppatch.com>
2013-07-26 15:09:42 -0400

    Add banner and clean section

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,13 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        'pkg': grunt.file.readJSON('package.json'),
+        'banner': '/**\n' +
+                  '* <%= pkg.name %> v<%= pkg.version %>\n' +
+                  '* Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                  '*/\n',
+        'clean': {
+            'dist': ['dist'],
+            'bower-deps': ['app/js/vendor']
+        }
     });
 };
