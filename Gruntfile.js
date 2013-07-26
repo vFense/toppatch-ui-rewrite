9d5791f0d730a27c30d9d4a7e3fcdd28fd90b1b7
Luis Aleman <Luis@toppatch.com>
2013-07-26 17:05:21 -0400

    Remove uneeded single quotes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,13 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        'pkg': grunt.file.readJSON('package.json'),
-        'banner': '/**\n' +
-                  '* <%= pkg.name %> v<%= pkg.version %>\n' +
-                  '* Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-                  '*/\n',
-        'clean': {
-            'dist': ['dist'],
-            'bower-deps': ['app/js/vendor']
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/**\n' +
+                '* <%= pkg.name %> v<%= pkg.version %>\n' +
+                '* Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                '*/\n',
+        clean: {
+            dist: ['dist'],
+            bowerDeps: ['app/js/vendor']
         }
     });
 };
