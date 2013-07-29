0bc51edc6977a466ab561fd5a292b0b3adeddb49
Luis Aleman <Luis@toppatch.com>
2013-07-29 11:05:11 -0400

    Add base structure to config.js, and comment on the function of each section

--- app/js/config.js
+++ app/js/config.js
@@ -1,4 +1,21 @@
 requirejs.config({
+    // baseURL: The root path to use for all module lookups.
+    // See: http://requirejs.org/docs/api.html#config-baseUrl
     baseUrl: './',
-    paths: {}
+
+    // paths: Path mappings for module names not found directly under baseUrl.
+    // See: http://requirejs.org/docs/api.html#config-paths
+    paths: {},
+
+    // shim: Configure dependencies, exports, and custom initialization for non-AMD scripts.
+    // See: http://requirejs.org/docs/api.html#config-shim
+    shim: {},
+
+    // deps: An array of dependencies to load as soon as require() is defined.
+    // See: http://requirejs.org/docs/api.html#config-deps
+    deps: (function() {}()),
+
+    // callback: A function to execute after deps have been loaded.
+    // See: http://requirejs.org/docs/api.html#config-callback
+    callback: function () { 'use strict'; return this; }
 });
