d3242f4c3557828b6180016bf46905432ec1ef9b
Luis Aleman <Luis@toppatch.com>
2013-07-29 12:13:37 -0400

    Add toppatch.com link to the gruntfile.js banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,12 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        banner: '/**\n' +
-                '* <%= pkg.name %> v<%= pkg.version %>\n' +
-                '* Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-                '*/\n',
+        banner:
+            '/**\n' +
+            ' * <%= pkg.name %> v<%= pkg.version %>\n' +
+            ' * www.toppatch.com\n *\n' +
+            ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' */\n',
         clean: {
             dist: ['dist']
         },
