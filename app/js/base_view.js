daa25ca7f699d57c21cd9f09818599ea531237bb
Luis Aleman <Luis@toppatch.com>
2013-07-30 13:35:44 -0400

    New base_view.js file
    Will include all global Backbone.View overrides

---
+++ app/js/base_view.js
@@ -0,0 +1,12 @@
+/**
+ * base_view.js
+ * ------------------------------------------------------------------------
+ * Backbone.View function overrides
+ *
+ * Includes:
+ *  Zombie Prevention
+ *  Close method
+ */
+define(function () {
+    "use strict";
+});
