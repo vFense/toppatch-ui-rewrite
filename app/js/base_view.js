8d1ef749e6389654b363b9297b2e1574456c2df6
Luis Aleman <Luis@toppatch.com>
2013-07-30 20:50:39 -0400

    Add View.close method

--- app/js/base_view.js
+++ app/js/base_view.js
@@ -13,6 +13,28 @@ define(
         "use strict";
         var __super__ = Backbone.View.prototype;
         _.extend(__super__, {
+            // Zombie Prevention Part 1
+            // Add close function to Backbone.View to prevent "Zombies"
+            // Inspired by: Derick Bailey
+            // See: http://bit.ly/odAfKo
+            close: function () {
+                if (this.isClosing) { return this; }
+                if (this.beforeClose && _.isFunction(this.beforeClose)) {
+                    this.beforeClose();
+                }
+                this.isClosing = true;
+                if (_.isArray(this.childViews)) {
+                    _.each(this.childViews, function (childView) {
+                        if (_.isFunction(childView.close)) {
+                            childView.close();
+                        }
+                    });
+                }
+                this.remove();
+                this.unbind();
+                this.isClosing = false;
+                return this;
+            },
         });
     }
 );
