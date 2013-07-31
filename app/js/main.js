6e23473b95be2c85bc4c0c570cdab8980f89eab7
Luis Aleman <Luis@toppatch.com>
2013-07-30 23:10:21 -0400

    Ensure base_deps are loaded up front

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
-define(function() {
+define(['js/base_deps'], function() {
     "use strict";
-    require(['js/base_deps']);
     require(['js/dashboard_view'], function (Dashboard) {
         var $dash = new Dashboard();
         return $dash.render().showLoading();
