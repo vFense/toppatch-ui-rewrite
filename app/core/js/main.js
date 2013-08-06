17392d32fd8bf3f26c5d41d878119060bf6aa633
Luis Aleman <Luis@toppatch.com>
2013-08-06 15:01:39 -0400

    rename dashboard_view.js -> dashboard.js

--- app/core/js/main.js
+++ app/core/js/main.js
@@ -1,6 +1,6 @@
 define(['core/js/base_deps'], function() {
     "use strict";
-    require(['core/js/dashboard_view'], function (Dashboard) {
+    require(['core/js/dashboard'], function (Dashboard) {
         App.dashboard_view = new Dashboard();
         return App.dashboard_view.render().showLoading();
     });
