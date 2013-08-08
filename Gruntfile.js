f8e175eb937c3b1832fef097cb2e5564f02a00ed
Luis Aleman <Luis@toppatch.com>
2013-08-08 11:09:47 -0400

    Sort the task configuration alphabetically

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,24 +15,26 @@ module.exports = function(grunt) {
             ' * www.toppatch.com\n *\n' +
             ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
             ' */\n',
-        watch: {
-            options: {
-                livereload: true
-            },
-            application: {
-                files: ['<%= rv.app %>/less/*.less'],
-                tasks: ['recess:application']
-            },
-            bootstrap: {
-                files: ['<%= rv.app %>/vendor/bootstrap/less/*.less'],
-                tasks: ['recess:bootstrap']
-            },
-            javascript: {
-                files: ['<%= rv.app %>/core/js/*.js']
-            },
-            html: {
-                files: ['<%= rv.app %>/*.html', '<%= rv.app %>/core/templates/**/*.html']
-            }
+
+        /**********************
+         * Task Configuration *
+         **********************/
+
+        clean: {
+            dist: ['dist']
+        },
+        concurrent: {
+            dev: [
+                'recess:application',
+                'recess:bootstrap'
+            ],
+            dist: [
+                'recess:dist',
+                'imagemin:dist',
+                'uglify:dist',
+                'requirejs:dist',
+                'targethtml:dist'
+            ]
         },
         connect: {
             server: {
@@ -41,14 +43,6 @@ module.exports = function(grunt) {
                     hostname: '*'
                 }
             }
-        },
-        open: {
-            dev: {
-                url: 'http://localhost:8000/'
-            }
-        },
-        clean: {
-            dist: ['dist']
         },
         copy: {
             dist: {
@@ -59,13 +53,6 @@ module.exports = function(grunt) {
                 ]
             }
         },
-        uglify: {
-            dist: {
-                files: {
-                    '<%= rv.dist %>/js/modernizr.min.js': ['<%= rv.app %>/vendor/modernizr/modernizr.js']
-                }
-            }
-        },
         imagemin: {
             dist: {
                 files: [{
@@ -74,6 +61,11 @@ module.exports = function(grunt) {
                     src: '{,*/}*.{png,jpg,jpeg}',
                     dest: '<%= rv.dist %>/images'
                 }]
+            }
+        },
+        open: {
+            dev: {
+                url: 'http://localhost:8000/'
             }
         },
         recess: {
@@ -128,18 +120,31 @@ module.exports = function(grunt) {
                 }
             }
         },
-        concurrent: {
-            dev: [
-                'recess:application',
-                'recess:bootstrap'
-            ],
-            dist: [
-                'recess:dist',
-                'imagemin:dist',
-                'uglify:dist',
-                'requirejs:dist',
-                'targethtml:dist'
-            ]
+        uglify: {
+            dist: {
+                files: {
+                    '<%= rv.dist %>/js/modernizr.min.js': ['<%= rv.app %>/vendor/modernizr/modernizr.js']
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            application: {
+                files: ['<%= rv.app %>/less/*.less'],
+                tasks: ['recess:application']
+            },
+            bootstrap: {
+                files: ['<%= rv.app %>/vendor/bootstrap/less/*.less'],
+                tasks: ['recess:bootstrap']
+            },
+            javascript: {
+                files: ['<%= rv.app %>/core/js/*.js']
+            },
+            html: {
+                files: ['<%= rv.app %>/*.html', '<%= rv.app %>/core/templates/**/*.html']
+            }
         }
     });
 
