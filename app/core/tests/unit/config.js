00671a0232bf51bf029299356c4c41c3879b844c
Luis Aleman <Luis@toppatch.com>
2013-08-12 16:11:03 -0400

    Improve require.config tests

--- app/core/tests/unit/config.js
+++ app/core/tests/unit/config.js
@@ -1,24 +1,71 @@
 $(document).ready(function () {
     "use strict";
     module('require.config');
-    test("Test RequireJS paths and shims", function () {
-        stop();
-        require(['text'], function () {
-            var paths = requirejs_paths;
-            $.each(paths, function (key) {
-                stop();
-                require(
-                    [key],
-                    function (input) {
-                        ok(true, [key + ' -> ', key, '.js'].join(''));
-                        start();
-                    },
-                    function (err) { ok(false, err); start(); }
-                );
-            });
-            start();
+    test("For every shim, there must be an equally named path (shim[name] && path[name] !== undefined)", function() {
+        var paths = requirejs_paths,
+            shims = requirejs_shims;
+
+        $.each(shims, function(shim) {
+            notStrictEqual(typeof paths[shim], 'undefined', shim);
         });
     });
+    test("For every shim dependency, there must be an equally named path (shim.deps[name] && path[name] !== undefined)", function () {
+        var paths = requirejs_paths,
+            shims = requirejs_shims;
 
-    start();
+        $.each(shims, function(shim, shimProps) {
+            if (typeof shimProps.deps !== 'undefined') {
+                var pass = true;
+                $.each(shimProps.deps, function(index, dependency) {
+                    if (typeof paths[dependency] === 'undefined') {
+                        ok(false, [shim, ' - Dependency "', dependency, '" is undefined'].join(''));
+                        pass &= false;
+                    }
+                });
+                if (pass) {
+                    ok(pass, [shim, 'has undefined dependency paths'].join(' '));
+                }
+            }
+        });
+    });
+    asyncTest("Attempt to require all paths", function () {
+        /**
+         * Previous versions of this test had race condition in which two versions of
+         * bootstrap.tooltip was loading at the same time. If the version called by
+         * bootstrap.popover was not loaded first, bootstrap.popover would throw
+         * an uncaught exception.
+         *
+         * This version gets around this by waiting for each require call to complete
+         * before calling the next require call.
+         */
+        var paths = _.keys(requirejs_paths),
+            pathCount = paths.length,
+            pathTester,
+            nextPath,
+            i = 0;
+
+        nextPath = function () {
+            if (i < pathCount) {
+                pathTester(paths[i++], nextPath);
+            } else {
+                start();
+            }
+        };
+
+        pathTester = function(path, callback) {
+            require(
+                [path],
+                function () {
+                    ok(true, path);
+                    nextPath();
+                },
+                function (error) {
+                    ok(false, [path, '-', error.message].join(' '));
+                    nextPath();
+                }
+            );
+        };
+
+        nextPath();
+    });
 });
