4707d8a5c7e9fc4b906b47fdf906e5532c6401c1
Luis Aleman <Luis@toppatch.com>
2013-08-15 12:17:50 -0400

    Add comments to clarify the code

--- app/core/js/base_view.js
+++ app/core/js/base_view.js
@@ -22,14 +22,22 @@ define(
             // Inspired by: Derick Bailey
             // See: http://bit.ly/odAfKo
             close: function () {
+                // Only proceed if not already closing
                 if (!this.isClosing) {
+                    // Declare that this view is closing
                     this.isClosing = true;
+
+                    // Call before close if it is a function
                     if (this.beforeClose && _.isFunction(this.beforeClose)) {
                         this.beforeClose();
                     }
+
+                    // Close child views, then remove and unbind this view
                     this.closeChildViews()
                         .remove()
                         .unbind();
+
+                    // Closing has completed
                     this.isClosing = false;
                 }
                 return this;
