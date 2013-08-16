ab894365561cfc389ed5c291bbcc0426a2a4c8dc
Luis Aleman <Luis@toppatch.com>
2013-08-16 15:40:22 -0400

    Drop some comments
    Hopefully this code is short enough to be self-explanatory

--- app/core/js/base_router.js
+++ app/core/js/base_router.js
@@ -2,20 +2,16 @@
  * base_router.js
  * ------------------------------------------------------------------------
  * Backbone.Router function overrides
- *
- * Includes:
  */
 define(['backbone'], function () {
     'use strict';
     return Backbone.Router.extend({
-        // Constructor override to track fragments on route event
         constructor: function(){
             Backbone.Router.prototype.constructor.apply(this, _.toArray(arguments));
             this.on('route', this.updateLastFragment);
             return this;
         },
 
-        // Fragment Tracking
         lastFragment: null,
         currentFragment: null,
         updateLastFragment: function () {
