cd0f3237cc600444597a3f47efc5705b0d488b92
Luis Aleman <Luis@toppatch.com>
2013-08-22 13:36:25 -0400

    Region manager boilerplate

--- app/core/js/base_regionManager.js
+++ app/core/js/base_regionManager.js
@@ -5,3 +5,36 @@
  *
  * Inspired by Backbone.Marionette.regionManager
  */
+define(
+    ['core/js/base_region'],
+    function (Region) {
+        'use strict';
+        /**
+         * Creates an instance of RegionManager
+         * @returns {this}
+         * @constructor
+         */
+        var RegionManager = function () {
+            this._regions = {};
+            this.length = 0;
+            return this;
+        };
+
+        _.extend(RegionManager.prototype, {
+        });
+
+        // The following code is inspired by Backbone's source code
+        // See http://backbonejs.org/docs/backbone.html#section-71 (As of 08/22/2013)
+        // Underscore methods that we want to implement on the RegionManager
+        var regionMethods = [];
+        _.each(regionMethods, function(method) {
+            RegionManager.prototype[method] = function() {
+                var args = _.toArray(arguments);
+                args.unshift(this._regions);
+                return _[method].apply(_, args);
+            };
+        });
+
+        return RegionManager;
+    }
+);
