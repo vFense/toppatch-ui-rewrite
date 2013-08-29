2a8ed3decfa7a48a59671bc924cc2b37de33290b
Luis Aleman <Luis@toppatch.com>
2013-08-29 17:15:11 -0400

    Drop outdated test

--- app/core/tests/unit/base_loader.js
+++ app/core/tests/unit/base_loader.js
@@ -10,10 +10,8 @@ $(document).ready(function () {
 
                 strictEqual(loader.render(), loader, 'Render method returns this');
 
-                var $spinner = loader.$('.spinner'),
-                    $text = loader.$('[data-name="text"]');
+                var $text = loader.$('[data-name="text"]');
 
-                strictEqual($spinner.length, 3, 'Three spinner layers have been rendered');
                 strictEqual($text.length, 1, 'The loading text area has been rendered');
                 strictEqual($text.text(), '', 'The loading text area is blank by default');
 
