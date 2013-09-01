9e3f5f81c3d81e05ef251f7a38bd07e70301e4d6
Luis Aleman <Luis@toppatch.com>
2013-09-01 00:26:29 -0400

    New render method, and test

--- app/core/js/templateView.js
+++ app/core/js/templateView.js
@@ -10,6 +10,26 @@ define(
     function (BaseView) {
         'use strict';
         return BaseView.extend({
+            render: function () {
+                this.isClosed = false;
+                if (_.isFunction(this.template)) {
+                    var data = this.getData(),
+                        html = this.template(data);
+                    this.$el.html(html);
+                } else {
+                    var error;
+                    if (_.isUndefined(this.template)) {
+                        error = new Error('Template is undefined');
+                        error.name = 'TemplateUndefined';
+                    } else {
+                        error = new TypeError('Expected template to be a function');
+                        error.name = 'TemplateNotFunction';
+                    }
+                    throw error;
+                }
+                // Base view does not have a render method
+                return this;
+            },
             getData: function () {
                 var data = {};
                 if (this.model instanceof Backbone.Model) {
--- app/core/tests/unit/templateView.js
+++ app/core/tests/unit/templateView.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     'use strict';
     module('TemplateView');
-
     asyncTest('getData', function () {
         require(
             ['core/js/templateView'],
@@ -33,6 +32,55 @@ $(document).ready(function () {
                 view.collection.add(set);
                 view.model = undefined;
                 deepEqual(view.getData(), { items: set }, 'Got collection, when no model is present');
+
+                start();
+            }
+        );
+    });
+
+    asyncTest('render', function () {
+        require(
+            ['handlebars', 'core/js/templateView'],
+            function (Handlebars, TemplateView) {
+                var Model = Backbone.Model.extend({
+                    defaults: {
+                        name: 'World'
+                    }
+                });
+
+                // The following is a pre-compiled template of:
+                // <div>Hello {{ name }}!</div>
+                var template = Handlebars.template(function(Handlebars, depth0, helpers, partials, data) {
+                    this.compilerInfo = [ 4, '>= 1.0.0' ];
+                    helpers = this.merge(helpers, Handlebars.helpers);
+                    data = data || {};
+                    var buffer = '', stack1, functionType = 'function', escapeExpression = this.escapeExpression;
+                    buffer += '<div>Hello ';
+                    stack1 = helpers.name;
+                    if (stack1) {
+                        stack1 = stack1.call(depth0, {
+                            hash: {},
+                            data: data
+                        });
+                    } else {
+                        stack1 = depth0.name;
+                        stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1;
+                    }
+                    buffer += escapeExpression(stack1) + '!</div>';
+                    return buffer;
+                });
+
+                var view = new TemplateView();
+                view.model = new Model();
+
+                QUnit.throws(function () { view.render(); }, Error, 'Render without template threw an Error');
+
+                view.template = {};
+                QUnit.throws(function () { view.render(); }, TypeError, 'Render with template that is not a function, threw a TypeError');
+
+                view.template = template;
+                view.render();
+                strictEqual(view.$el.html(), '<div>Hello World!</div>', 'Template rendered correctly');
 
                 start();
             }
