2daaddc711c106310fb14012660b1e4d1394eaeb
Luis Aleman <Luis@toppatch.com>
2013-09-05 11:03:48 -0400

    Make loaderView extend templateView

--- app/core/js/loaderView.js
+++ app/core/js/loaderView.js
@@ -1,14 +1,10 @@
 define(
-    ['core/js/view', 'core/js/template/loading_indicator'],
+    ['core/js/templateView', 'core/js/template/loading_indicator'],
     function (BaseView, template) {
         'use strict';
         return BaseView.extend({
             className: 'loading_indicator',
             template: template,
-            render: function () {
-                this.$el.html(this.template());
-                return this;
-            },
             setLoadingText: function (text) {
                 this.$('[data-name="text"]').text(text);
                 return this;
