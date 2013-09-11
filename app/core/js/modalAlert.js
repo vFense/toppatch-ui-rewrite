1865c139b501c0c48a0d4e8ba99ab703dffdab22
Luis Aleman <Luis@toppatch.com>
2013-09-10 20:55:45 -0400

    Save latest changes

--- app/core/js/modalAlert.js
+++ app/core/js/modalAlert.js
@@ -28,13 +28,13 @@ define(
         var AlertModel = Backbone.Model.extend({
             defaults: {
                 icon: null,
-                titleField: '',
-                messageField: '',
+                message: '',
+                information: '',
                 defButton: null,
                 altButton: null,
                 othButton: null
             },
-            fieldNames: ['titleField', 'messageField'],
+            fieldNames: ['message', 'information'],
             buttonNames: ['defButton', 'altButton', 'othButton']
         });
 
@@ -84,36 +84,28 @@ define(
 
             setButtons: function (buttons) {
                 var count = buttons.length;
-                this.model.set({
-                    'defButton': count > 0 ? buttons[0] : null,
-                    'altButton': count > 1 ? buttons[1] : null,
-                    'othButton': count > 2 ? buttons[2] : null
-                });
+                this.setButton('defButton', count > 0 ? buttons[0] : null);
+                this.setButton('altButton', count > 1 ? buttons[1] : null);
+                this.setButton('othButton', count > 2 ? buttons[2] : null);
                 return this;
             },
 
-            setKeyEquivalent: function (button) {
-                var title = button.title;
-                if (button.keyEquivalent !== '\r') {
-                    if (title.toLowerCase() === 'cancel') {
-                        // Any button with a title of “Cancel”
-                        // has a key equivalent of Escape (\x1B)
-                        button.keyEquivalent = '\x1B';
-                    } else {
-                        button.keyEquivalent = '';
-                    }
+            setMessage: function (text) {
+                if (_.isString(text)) {
+                    this.model.set({message: text});
                 }
                 return this;
             },
 
-            result: null,
-            buttonAction: function (event) {
-                $.noop(event);
+            setInformation: function (text) {
+                if (_.isString(text)) {
+                    this.model.set({information: text});
+                }
                 return this;
             },
 
-            resetAlert: function () {
-                this.model = new AlertModel();
+            buttonAction: function (event) {
+                $.noop(event);
                 return this;
             }
         });
@@ -123,9 +115,21 @@ define(
          * @constructor
          */
         var ModalAlert = function () {
-            this.alertView = new AlertView();
+            var Collection = Backbone.Collection.extend({
+                model: AlertButton
+            });
+            _.extend(this, {
+                _messageText: 'Alert',
+                _informativeText: '',
+                _icon: '',
+                _buttons: new Collection()
+            });
+            return this;
         };
 
+        /**
+         * Set some non-instanced variables
+         */
         _.extend(ModalAlert, {
             FirstButtonReturn: 1000,
             SecondButtonReturn: 1001,
@@ -137,19 +141,15 @@ define(
             AlertView: AlertView
         });
 
+        /**
+         * Set instance variables
+         */
         _.extend(ModalAlert.prototype, {
-            // Alert View Attributes
-            _alertStyle: 'info',
-            _messageText: 'Alert',
-            _informativeText: '',
-            _icon: '',
-            _buttons: [],
-
             alertWithMessage: function (messageText, defButtonTitle, altButtonTitle, otherButtonTitle, informativeText) {
                 this.setMessageText(messageText);
 
-                if (_.isString(information)) {
-                    this.setInformativeText(information);
+                if (_.isString(informativeText)) {
+                    this.setInformativeText(informativeText);
                 }
 
                 if (_.isString(defButtonTitle)) {
@@ -163,13 +163,25 @@ define(
                 return this;
             },
 
-            setMessageText: function (text) {
-                this._messageText = text.trim();
+            _setText: function (reference, text) {
+                if (!_.isUndefined(text) && _.isString(text)) {
+                    this[reference] = text.trim;
+                }
+                return this[reference];
             },
 
-            setInformativeText: function (text) {
-                this._informativeText = text.trim();
-                return this;
+            messageText: function (text) {
+                if (_.isString(text)) { this._messageText = text.trim; }
+                return this._messageText;
+            },
+
+            informativeText: function (text) {
+                if (_.isString(text)) { this._informativeText = text.trim; }
+                return this._informativeText;
+            },
+
+            icon: function () {
+                return this._icon;
             },
 
             addButtonWithTitle: function (title) {
@@ -185,22 +197,40 @@ define(
                     button.tag = ModalAlert.FirstButtonReturn;
                     button.keyEquivalent = '\r';
                 } else {
-                    button['btn-style'] = 'btn-default';
                     button.tag = ModalAlert.FirstButtonReturn + count;
                     this.setKeyEquivalent(button);
                 }
 
-                this.buttons.push(button);
+                this._buttons.add(button);
                 return button;
             },
 
-            setAlertStyle: function (style) {
-                if (_.isString(style)) {
-                    this.$el
-                        .removeClass(this.alertStyle)
-                        .addClass(style);
-                    this.alertStyle = style;
+            setKeyEquivalent: function (button) {
+                var title = button.title;
+                if (button.keyEquivalent !== '\r') {
+                    if (title.toLowerCase() === 'cancel') {
+                        // Any button with a title of “Cancel”
+                        // has a key equivalent of Escape (\x1B)
+                        button.keyEquivalent = '\x1B';
+                    } else {
+                        button.keyEquivalent = null;
+                    }
                 }
+                return this;
+            },
+
+            buttons: function () {
+                return this._buttons;
+            },
+
+            _setupAlert: function () {
+                if (this._alertView instanceof DialogView) {
+                    this._alertView.close();
+                }
+                var alert = this._alertView = new AlertView();
+                alert.setMessage(this._messageText)
+                     .setInformation(this._informativeText)
+                     .setButtons(this._buttons);
                 return this;
             }
         });
