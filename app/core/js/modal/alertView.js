2d0ecdb4708997f4b7bbe67721aa961ef4fbcf80
Luis Aleman <Luis@toppatch.com>
2013-09-16 15:18:46 -0400

    Drop AlertModel and update for the new version of Button

--- app/core/js/modal/alertView.js
+++ app/core/js/modal/alertView.js
@@ -6,21 +6,23 @@ define(
     function (DialogView, alertTemplate, Button) {
         'use strict';
 
-        var AlertModel = Backbone.Model.extend({
-            defaults: {
-                icon: null,
-                message: '',
-                information: '',
-                defButton: null,
-                altButton: null,
-                othButton: null
-            },
-            fieldNames: ['message', 'information'],
-            buttonNames: ['defButton', 'altButton', 'othButton']
-        });
-
         return DialogView.extend({
             template: alertTemplate,
+
+            constructor: function () {
+                _.extend(this, {
+                    // Alert properties
+                    icon: null,
+                    message: '',
+                    information: '',
+                    buttons: {
+                        default: null,
+                        alternate: null,
+                        other: null
+                    }
+                });
+                return DialogView.prototype.constructor.apply(this, arguments);
+            },
 
             attributes: function () {
                 return _.extend({}, _.result(DialogView.prototype, 'attributes'), {
@@ -40,36 +42,28 @@ define(
              */
             events: function () {
                 return _.extend({}, _.result(DialogView.prototype, 'events'), {
-                    'click BUTTON:enabled': 'buttonAction',
-                    'keydown': 'keyAction'
+                    'click BUTTON:enabled': 'clickAction',
+                    'keypress': 'keyAction'
                 });
             },
 
-            /**
-             * Construct this DialogView
-             * @constructor
-             * @returns {number}
-             */
-            constructor: function () {
-                this.model = new AlertModel();
-                return DialogView.prototype.constructor.apply(this, arguments);
+            getData: function () {
+                return _.extend({}, _.pick(this, 'icon', 'message', 'information', 'buttons'));
             },
 
             /**
              * Set a button instance
              * @param name {string} Name of the button to set
-             * @param button {core/js/button.View|null}
+             * @param button {core/js/button|null}
              * @returns {this}
              */
             setButton: function (name, button) {
                 // Short circuit only set for names we want
-                if (_.contains(this.model.buttonNames, name)) {
-                    if (_.isNull(button)) {
-                        this.model.set(name, null);
-                    } else if (button instanceof Button.View) {
-                        this.model.set(name, button);
+                if (_.has(this.buttons, name)) {
+                    if (_.isNull(button) || (button instanceof Button)) {
+                        this.buttons[name] = button;
                     } else {
-                        throw new TypeError('Expected button to be instance of Button.View or null');
+                        throw new TypeError('Expected button to be instance of Button or null');
                     }
                 }
                 return this;
@@ -83,45 +77,52 @@ define(
             setButtons: function (buttons) {
                 if (_.isArray(buttons)) {
                     var count = buttons.length;
-                    this.setButton('defButton', count > 0 ? buttons[0] : null);
-                    this.setButton('altButton', count > 1 ? buttons[1] : null);
-                    this.setButton('othButton', count > 2 ? buttons[2] : null);
+                    this.setButton('default',   count > 0 ? buttons[0] : null);
+                    this.setButton('alternate', count > 1 ? buttons[1] : null);
+                    this.setButton('other',     count > 2 ? buttons[2] : null);
                 }
                 return this;
             },
 
+            /**
+             * Convenience method to set this alert.model's message
+             * @param text
+             * @returns {*}
+             */
             setMessage: function (text) {
                 if (_.isString(text)) {
-                    this.model.set({message: text.trim()});
+                    this.message = text.trim();
                 }
                 return this;
             },
 
+            /**
+             * Convenience method to set this alert.model's information
+             * @param text
+             * @returns {*}
+             */
             setInformation: function (text) {
                 if (_.isString(text)) {
-                    this.model.set({information: text});
+                    this.information = text.trim();
                 }
                 return this;
             },
 
+            /**
+             * On every keypress, run each button's performKeyEquivalent method
+             * @param event
+             * @returns {*}
+             */
             keyAction: function (event) {
-                var keyCode = event.which, // jQuery normalizes the 'which' property
-                    buttons = this.model.pick(this.model.buttonNames),
-                    match = _.find(buttons, function (button) {
-                        return _.result(button, 'keyEquivalent') === keyCode;
-                    });
-
-                if (_.isObject(match)) {
-                    this._result = match.tag;
-                    this.trigger('result', this._result);
-                    this.hide();
-                }
+                // Run each button's performKeyEquivalent
+                // Funky things will happen if there is more than one matching button
+                _.invoke(this.buttons, 'performKeyEquivalent', event);
 
                 return this;
             },
 
             _result: null,
-            buttonAction: function (event) {
+            clickAction: function (event) {
                 this._result = $(event.target).data('tagid');
                 this.trigger('result', this._result);
                 this.hide();
@@ -136,9 +137,9 @@ define(
              * @returns {*}
              */
             open: function () {
-                if (this.model.get('message') === '') {
+                if (this.message === '') {
                     throw new Error('Cannot open Alert: Alert message is empty');
-                } else if (_.isNull(this.model.get('defButton'))) {
+                } else if (_.isNull(this.buttons.def)) {
                     throw new Error('Cannot open Alert: DefaultButton is null');
                 }
                 DialogView.prototype.open.apply(this, arguments);
@@ -148,27 +149,21 @@ define(
             /**
              * Close this View
              * ---
-             * Currently this  method also conducts a full reset of this.model
-             * Which would prevent the reuse of this view. Is this the best
-             * solution? Is there a use case for reusing the view?
+             * Currently this method removes the button elements, and sets each
+             * button reference to null. This would cause the View to not be reusable
+             * without further setup.
+             * Is there a use case for reusing the view?
              *
              * @override
              * @returns {*}
              */
             close: function () {
                 if (!this.isClosed) {
-                    // Close/Remove the buttons views
-                    var buttons = _.pick(this.model.attributes, this.model.buttonNames);
-                    _.each(buttons, function (button) {
-                        if (_.isFunction(button.close)) {
-                            button.close();
-                        } else if (_.isFunction(button.remove)) {
-                            button.remove();
-                        }
+                    // Remove the buttons for garbage collection
+                    _.each(this.buttons, function (button) {
+                        _.result(button, 'remove');
+                        button = null;
                     }, this);
-
-                    // Reset this.model to its default state
-                    this.model.clear().set(this.model.defaults);
 
                     // Call the prototype close method
                     DialogView.prototype.close.apply(this, arguments);
