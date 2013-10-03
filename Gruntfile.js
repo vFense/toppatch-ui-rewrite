830ce7aee2160c2ecb58d461f7f417d24432a000
Luis Aleman <Luis@toppatch.com>
2013-10-03 17:15:35 -0400

    Attempt to DRY the grunt tasks
    Able to drop grunt-concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
          **********************/
 
         clean: {
-            app: [
+            dev: [
                 '<%= meta.app %>css',
                 '<%= meta.app %>core/template/**/*.js',
                 '<%= meta.app %>fonts'
@@ -34,24 +34,15 @@ module.exports = function(grunt) {
             temp        : ['<%= meta.temp %>'],
             vendor      : ['<%= meta.app %>vendor']
         },
-        concat: {
-            options: {
-                stripBanners: true,
-                banner: '<%= banner %>'
-            },
-            dist: {
-                src: '<%= meta.dist %>js/toppatch-ui.js',
-                dest: '<%= meta.dist %>js/toppatch-ui.js'
-            }
-        },
         concurrent: {
             dev: [
+                'copy:dev',
                 'less:application',
                 'less:bootstrap',
                 'templates'
             ],
             dist: [
-                'less:dist',
+                'copy:dist',
                 'imagemin:dist',
                 'uglify:modernizr',
                 'requirejs:dist',
@@ -85,10 +76,10 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: [
-                    { cwd: '<%= meta.app %>', src: '404.html', dest: '<%= meta.dist %>', expand: true },
-                    { cwd: '<%= meta.app %>', src: 'robots.txt', dest: '<%= meta.dist %>', expand: true },
-                    { cwd: '<%= meta.app %>', src: 'images/*', dest: '<%= meta.dist %>', expand: true },
-                    { cwd: '<%= meta.app %>vendor/bootstrap/', src: 'fonts/*', dest: '<%= meta.dist %>', expand: true }
+                    { cwd: '<%= meta.app %>', dest: '<%= meta.dist %>', expand: true, src: 'robots.txt' },
+                    { cwd: '<%= meta.app %>', dest: '<%= meta.dist %>', expand: true, src: 'images/*'   },
+                    { cwd: '<%= meta.app %>', dest: '<%= meta.dist %>', expand: true, src: 'css/*'      },
+                    { cwd: '<%= meta.app %>', dest: '<%= meta.dist %>', expand: true, src: 'fonts/*'    }
                 ]
             },
             docs: {
@@ -103,9 +94,9 @@ module.exports = function(grunt) {
             },
             dev: {
                 expand: true,
-                cwd: '<%= meta.temp %>',
-                src: 'template/**/*.hbs',
-                dest:'<%= meta.temp %>/',
+                cwd: '<%= meta.temp %>/template',
+                src: '**/*.hbs',
+                dest:'<%= meta.app %>/core/template/',
                 ext: '.js'
             }
         },
@@ -128,7 +119,7 @@ module.exports = function(grunt) {
             dist: {
                 files: [{
                     expand: true,
-                    cwd: '<%= meta.dist %>images',
+                    cwd: '<%= meta.app %>images',
                     src: '**/*.{png,jpg,jpeg,gif}',
                     dest: '<%= meta.dist %>images'
                 }]
@@ -161,15 +152,6 @@ module.exports = function(grunt) {
             bootstrap: {
                 files: {
                     '<%= meta.app %>css/bootstrap.css'  : ['<%= meta.app %>vendor/bootstrap/less/bootstrap.less']
-                }
-            },
-            dist: {
-                options: {
-                    yuicompress: true
-                },
-                files: {
-                    '<%= meta.dist %>css/bootstrap.css'  : ['<%= meta.app %>vendor/bootstrap/less/bootstrap.less'],
-                    '<%= meta.dist %>css/application.css': ['<%= meta.app %>less/application.less']
                 }
             }
         },
@@ -204,19 +186,16 @@ module.exports = function(grunt) {
                     include: ['core/js/main'],
                     insertRequire: ['core/js/main'],
                     name: 'vendor/requirejs/require',
-                    optimize: 'uglify2',
-                    out: '<%= meta.dist %>js/toppatch-ui.js',
-                    uglify2: {
-                        'screw-ie8': true
-                    }
+                    optimize: 'none',
+                    out: '<%= meta.dist %>js/toppatch-ui.js'
                 }
             }
         },
         targethtml: {
             dist: {
-                files: {
-                    '<%= meta.dist %>index.html': '<%= meta.app %>index.html'
-                }
+                files: [
+                    { cwd: '<%= meta.app %>', dest: '<%= meta.dist %>', expand: true, src: '*.html' }
+                ]
             }
         },
         uglify: {
@@ -225,21 +204,14 @@ module.exports = function(grunt) {
                     '<%= meta.dist %>js/modernizr.min.js': ['<%= meta.app %>vendor/modernizr/modernizr.js']
                 }
             },
-            templates: {
+            dist: {
                 options: {
-                    beautify: {
-                        width: 80,
-                        beautify: true,
-                        bracketize: true
-                    },
-                    mangle: false,
-                    compress: false,
-                    'screw-ie8': true
+                    banner: '<%= meta.banner %>',
+                    sourceMap: '<%= meta.dist %>js/toppatch-ui.map.js'
                 },
-                expand: true,
-                cwd: '<%= meta.temp %>/template/',
-                src: '**/*.js',
-                dest: '<%= meta.app %>/core/template/'
+                files: {
+                    '<%= meta.dist %>js/toppatch-ui.js': ['<%= meta.dist %>js/toppatch-ui.js']
+                }
             }
         },
         watch: {
@@ -279,9 +251,9 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.registerTask('default', ['test', 'clean:dist', 'copy:dist', 'templates', 'concurrent:dist', 'concat']);
-    grunt.registerTask('dev', ['clean:app', 'copy:dev', 'concurrent:dev', 'connect:server', 'watch']);
+    grunt.registerTask('default', ['test', 'clean:dev', 'concurrent:dev', 'clean:dist', 'concurrent:dist', 'uglify:dist']);
+    grunt.registerTask('dev', ['clean:dev', 'concurrent:dev', 'connect:server', 'watch']);
     grunt.registerTask('docs', ['yuidoc', 'copy:docs']);
-    grunt.registerTask('templates', ['clean:temp', 'htmlmin:templates', 'handlebars:dev', 'uglify:templates', 'clean:temp']);
+    grunt.registerTask('templates', ['clean:temp', 'htmlmin:templates', 'handlebars:dev', 'clean:temp']);
     grunt.registerTask('test', ['jshint', 'connect:test', 'qunit']);
 };
