9b6ebc2c413a8de2e1b1318c9942b143eceaeb9c
Luis Aleman <Luis@toppatch.com>
2013-10-21 15:24:13 -0400

    Adjust chart tests

--- app/core/tests/unit/chart/chart.js
+++ app/core/tests/unit/chart/chart.js
@@ -9,13 +9,13 @@ $(document).ready(function () {
                 var chart;
 
                 ok(chart = new Chart(), 'Initialize new Chart with no options');
-                ok(_.has(chart.options, 'chart'), 'Chart options has chart key');
-                ok(_.has(chart.options.chart, 'renderTo'), 'Options has renderTo key');
-                strictEqual(_.result(chart.options.chart, 'renderTo'), chart.el, 'renderTo is set correctly');
-                ok(_.has(chart.options, 'title'), 'Chart options has title key');
-                ok(_.has(chart.options, 'credits'), 'Chart options has credits key');
-                ok(_.has(chart.options, 'xAxis'), 'Chart options has xAxis key');
-                ok(_.has(chart.options, 'yAxis'), 'Chart options has yAxis key');
+                ok(_.has(chart.chartOptions, 'chart'), 'Chart options has chart key');
+                ok(_.has(chart.chartOptions.chart, 'renderTo'), 'Options has renderTo key');
+                strictEqual(_.result(chart.chartOptions.chart, 'renderTo'), chart.el, 'renderTo is set correctly');
+                ok(_.has(chart.chartOptions, 'title'), 'Chart options has title key');
+                ok(_.has(chart.chartOptions, 'credits'), 'Chart options has credits key');
+                ok(_.has(chart.chartOptions, 'xAxis'), 'Chart options has xAxis key');
+                ok(_.has(chart.chartOptions, 'yAxis'), 'Chart options has yAxis key');
 
                 start();
             }
@@ -26,21 +26,77 @@ $(document).ready(function () {
         require(
             ['core/js/chart/chart'],
             function (Chart) {
-                var collection = new Backbone.Collection([
-                        {
-                            name: 'Tokyo',
-                            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-                        }
-                    ]),
-                    chart;
+                var collection, chart;
+                collection = new Backbone.Collection([
+                    {
+                        name: 'Tokyo',
+                        data: [7]
+                    }
+                ]);
 
                 ok(chart = new Chart({
                     collection: collection,
                 }), 'Initialize new Chart with sample collection');
 
                 ok(chart.render(), 'Render the chart');
+                ok(_.has(chart.$el.data(), 'highchartsChart'), '$el.data() has highcharts-chart attribute');
 
-                ok(_.has(chart.$el.data(), 'highchartsChart'), 'Highcharts instance created on chart\'s el');
+                start();
+            }
+        );
+    });
+
+    asyncTest('_destroyChart', function () {
+        require(
+            ['core/js/chart/chart'],
+            function (Chart) {
+                var collection, chart;
+                collection = new Backbone.Collection([
+                    {
+                        name: 'Tokyo',
+                        data: [7]
+                    }
+                ]);
+                chart = new Chart({
+                    collection: collection,
+                });
+
+                chart.render();
+                ok(chart._destroyChart(), 'Destroy the chart after render');
+                ok(!_.has(chart.$el.data(), 'highchartsChart'), '$el.data() does not have highcharts-chart attribute');
+                strictEqual(chart.$('*').length, 0, '$el is empty');
+
+                start();
+            }
+        );
+    });
+
+    asyncTest('close', function () {
+        require(
+            ['core/js/chart/chart'],
+            function (Chart) {
+                var collection, chart, called;
+                collection = new Backbone.Collection([
+                    {
+                        name: 'Tokyo',
+                        data: [7]
+                    }
+                ]);
+                chart = new Chart({
+                    collection: collection,
+                });
+
+                chart.render();
+
+                called = 0;
+                chart._destroyChart = function () {
+                    called += 1;
+                    return Chart.prototype._destroyChart.apply(chart, arguments);
+                };
+
+                ok(chart.close(), 'Close the chart after render');
+
+                strictEqual(called, 1, 'close method called _destroyChart');
 
                 start();
             }
