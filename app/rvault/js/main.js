30d9583b5d6a102bc16f94c2feedb542a4973014
Luis Aleman <Luis@toppatch.com>
2013-10-29 14:57:07 -0400

    MOve `core/dashboard.js` to `rvault/dashboard.js`

--- app/rvault/js/main.js
+++ app/rvault/js/main.js
@@ -1,6 +1,6 @@
 define(function() {
     'use strict';
-    require(['core/js/dashboard'], function (dashboard) {
+    require(['rvault/js/dashboard'], function (dashboard) {
         var dashboardView = new dashboard.View();
         return dashboardView.render();
     });
