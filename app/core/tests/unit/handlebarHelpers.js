4d67ccd927319ebd4a4ce03ff4324e79086e809c
Luis Aleman <Luis@toppatch.com>
2013-10-31 16:42:58 -0400

    Add and test compare helper

---
+++ app/core/tests/unit/handlebarHelpers.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+    'use strict';
+    module('handlebarHelpers');
+
+    asyncTest('Compare', function () {
+        require(
+            ['core/js/handlebarHelpers'],
+            function (Handlebars) {
+                var registered = _.has(Handlebars.helpers, 'compare'),
+                    options = {
+                        fn: function () { return true; },
+                        inverse: function () { return false; }
+                    },
+                    compare;
+                ok(registered, 'Compare helper is registered');
+
+                if(registered) {
+                    compare = Handlebars.helpers.compare;
+
+                    // Operation ==
+                    strictEqual(compare(1, '==', 1, options), true, '1 == 1 is true');
+                    strictEqual(compare(1, '==', '1', options), true, '1 == "1" is true');
+                    strictEqual(compare(1, '==', true, options), true, '1 == true is true');
+                    strictEqual(compare(1, '==', 0, options), false, '1 == 0 is false');
+
+                    // Operation !=
+                    strictEqual(compare(1, '!=', 1, options), false, '1 == 1 is false');
+                    strictEqual(compare(1, '!=', '1', options), false, '1 == "1" is false');
+                    strictEqual(compare(1, '!=', true, options), false, '1 == true is false');
+                    strictEqual(compare(1, '!=', 0, options), true, '1 == 0 is true');
+
+                    // Operation ===
+                    strictEqual(compare(1, '===', 1, options), true, '1 === 1 is true');
+                    strictEqual(compare(1, '===', '1', options), false, '1 === "1" is false');
+                    strictEqual(compare(1, '===', true, options), false, '1 === true is false');
+                    strictEqual(compare(1, '===', 0, options), false, '1 === 0 is false');
+
+                    // Operation !==
+                    strictEqual(compare(1, '!==', 1, options), false, '1 == 1 is false');
+                    strictEqual(compare(1, '!==', '1', options), true, '1 == "1" is true');
+                    strictEqual(compare(1, '!==', true, options), true, '1 == true is true');
+                    strictEqual(compare(1, '!==', 0, options), true, '1 == 0 is true');
+
+                    // Operation <
+                    strictEqual(compare(1, '<', 1, options), false, '1 < 1 is false');
+                    strictEqual(compare(1, '<', 2, options), true, '1 < 2 is true');
+                    strictEqual(compare(2, '<', 1, options), false, '2 < 1 is false');
+
+                    // Operation <=
+                    strictEqual(compare(1, '<=', 1, options), true, '1 <= 1 is true');
+                    strictEqual(compare(1, '<=', 2, options), true, '1 <= 2 is true');
+                    strictEqual(compare(2, '<=', 1, options), false, '2 <= 1 is false');
+
+                    // Operation >
+                    strictEqual(compare(1, '>', 1, options), false, '1 > 1 is false');
+                    strictEqual(compare(1, '>', 2, options), false, '1 > 2 is false');
+                    strictEqual(compare(2, '>', 1, options), true, '2 > 1 is true');
+
+                    // Operation >=
+                    strictEqual(compare(1, '>=', 1, options), true, '1 > 1 is true');
+                    strictEqual(compare(1, '>=', 2, options), false, '1 > 2 is false');
+                    strictEqual(compare(2, '>=', 1, options), true, '2 > 1 is true');
+
+                    strictEqual(compare(1, 'typeof', 'number', options), true, 'typeof 1 is number');
+                    strictEqual(compare(1, 'typeof', 'string', options), false, 'typeof 1 is not string');
+                }
+
+                start();
+            }
+        );
+    });
+});
