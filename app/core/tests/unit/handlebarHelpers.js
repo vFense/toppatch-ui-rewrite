12ddaca976f484850a20ed9311f2bb9a8b224d51
Luis Aleman <Luis@toppatch.com>
2013-10-31 16:51:27 -0400

    Add a few more testsw for 100% coverage

--- app/core/tests/unit/handlebarHelpers.js
+++ app/core/tests/unit/handlebarHelpers.js
@@ -17,6 +17,20 @@ $(document).ready(function () {
                 if(registered) {
                     compare = Handlebars.helpers.compare;
 
+                    throws(function () {
+                        compare();
+                    }, Error, 'Less than 3 arguments threw an error');
+
+                    throws(function () {
+                        compare(1, '/', 1, options);
+                    }, Error, 'Unknown comparison type');
+
+                    // Operation default (===)
+                    strictEqual(compare(1, 1, options), true, 'compare 1 1 is true');
+                    strictEqual(compare(1, '1', options), false, 'compare 1 "1" is false');
+                    strictEqual(compare(1, true, options), false, 'compare 1 true is false');
+                    strictEqual(compare(1, 0, options), false, 'compare 1 0 is false');
+
                     // Operation ==
                     strictEqual(compare(1, '==', 1, options), true, '1 == 1 is true');
                     strictEqual(compare(1, '==', '1', options), true, '1 == "1" is true');
