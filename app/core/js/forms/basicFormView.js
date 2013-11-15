14fc946f9ad40c0f380fecf066cb46d91f6ba153
Luis Aleman <Luis@toppatch.com>
2013-11-15 17:49:14 -0500

    New basicForm module, and tests

---
+++ app/core/js/forms/basicFormView.js
@@ -0,0 +1,74 @@
+/**
+ * A TemplateView that manages a Form
+ * @class basicFormView
+ * @extends TemplateView
+ */
+define(
+    ['../templateView'],
+    function (TemplateView) {
+        'use strict';
+
+        var formOptions = ['accept-charset', 'action', 'autocomplete', 'enctype', 'method', 'name', 'novalidate', 'target'];
+        return TemplateView.extend({
+            tagName: 'form',
+
+            /**
+             * A hash of attributes that will be set as HTML DOM element
+             * attributes on the view's el (id, class, data-properties, etc.),
+             * or a function that returns such a hash.
+             * @attribute attributes
+             * @type Object|Function
+             * @default Function
+             * @protected
+             */
+            attributes: function () {
+                return _.extend(
+                    {
+                        role: 'form'
+                    },
+                    _.pick(this.options, formOptions),
+                    _.result(TemplateView.prototype, 'attributes')
+                );
+            },
+
+            /**
+             * Listen for element events
+             * Uses a function to inherit events
+             * @attribute events
+             * @type Object|Function
+             * @default Function
+             * @protected
+             */
+            events: function () {
+                return _.extend(
+                    {
+                        'submit': 'submit'
+                    },
+                    _.result(TemplateView.prototype, 'events')
+                );
+            },
+
+            submit: function (event) {
+                if (_.isObject(event)) { event.preventDefault(); }
+                this.trigger('submit', this.serializeForm());
+                return this;
+            },
+
+            serializeForm: function () {
+                var output = {};
+                var input = this.$el.serializeArray();
+                $.each(input, function() {
+                    if (output[this.name] !== undefined) {
+                        if (!output[this.name].push) {
+                            output[this.name] = [output[this.name]];
+                        }
+                        output[this.name].push(this.value || '');
+                    } else {
+                        output[this.name] = this.value || '';
+                    }
+                });
+                return output;
+            },
+        });
+    }
+);
---
+++ app/core/tests/unit/forms/basicFormView.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    'use strict';
+    module('forms/basicFormView');
+
+    asyncTest('serializeForm', function () {
+        require(
+            ['core/js/forms/basicFormView'],
+            function (BasicForm) {
+                var form = new BasicForm({
+                    template: _.template([
+                        '<input type="text" name="field1" value="1"/>',
+                        '<input type="text" name="field2" value="2-1"/>',
+                        '<input type="text" name="field2" value="2-2"/>',
+                        '<input type="text" name="field3" value="3" disabled/>',
+                        '<select name="field4"><option value="4-1"></option><option value="4-2" selected></option></select>',
+                        '<input type="hidden" name="field5" value="5"></hidden>',
+                        '<input type="checkbox" name="field6"/>',
+                        '<input type="checkbox" name="field7" checked/>'
+                    ].join('\n'))
+                });
+
+                var data = form.render().serializeForm();
+                deepEqual(data, {
+                    field1: '1',
+                    field2: ['2-1','2-2'],
+                    field4: '4-2',
+                    field5: '5',
+                    field7: 'on'
+                }, 'Form serialized correctly');
+
+                ok(_.isUndefined(data.field3), 'Field 3 is undefined because the input is disabled');
+                ok(_.isUndefined(data.field6), 'Field 6 is undefined because the input is not checked');
+                start();
+            }
+        );
+    });
+
+    asyncTest('submit', function () {
+        require(
+            ['core/js/forms/basicFormView'],
+            function (BasicForm) {
+                var form = new BasicForm({
+                    template: _.template('<input type="text" name="field1" value="42"/>')
+                });
+
+                form.render();
+                form.once('submit', function (data) {
+                    ok(true, 'Submit event captured');
+
+                    ok(_.isObject(data), 'event passed data object');
+
+                    deepEqual(data, {
+                        field1: '42'
+                    }, 'serialized form is correct');
+
+                    start();
+                });
+
+                form.$el.submit();
+            }
+        );
+    });
+});
