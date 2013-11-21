3caebeff8cb7d94852f91e2050c2a74a03c045a7
Luis Aleman <Luis@toppatch.com>
2013-11-21 12:20:40 -0500

    Update the TopPatch module for use with the new global module
    The TopPatch module is responsible for populating the TopPatch global
    object

--- app/core/js/TopPatch/TopPatch.js
+++ app/core/js/TopPatch/TopPatch.js
@@ -1,32 +1,13 @@
-/**
- * Global object containing constants and other variables
- * @class TopPatch
- * @static
- */
 define(
     function (require) {
         'use strict';
-        var _TopPatch = window.TopPatch, TopPatch;
 
-        TopPatch = _.extend({},
+        var TopPatch = require('./global');
+
+        TopPatch.extend(
             require('./constants'),
-            require('./auth'),
-            {
-                /**
-                 * Relinquish TopPatch's control of the `window.TopPatch` variable.
-                 * @method noConflict
-                 * @returns TopPatch
-                 */
-                noConflict: function () {
-                    if (window.TopPatch === TopPatch) {
-                        window.TopPatch = _TopPatch;
-                    }
-                    return TopPatch;
-                }
-            }
+            require('./auth')
         );
-
-        window.TopPatch = TopPatch;
 
         return TopPatch;
     }
