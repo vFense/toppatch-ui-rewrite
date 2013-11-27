e369ff87e183a40baa534fd98cd27106f92b2208
Luis Aleman <Luis@toppatch.com>
2013-11-27 17:14:02 -0500

    Test authRoute methods

--- app/core/tests/unit/router.js
+++ app/core/tests/unit/router.js
@@ -81,4 +81,92 @@ $(document).ready(function () {
             start();
         });
     });
+
+    asyncTest('authRoute argument parsing', function () {
+        var testSuite = this;
+        require(['core/js/router', 'core/js/TopPatch/Auth'], function(Router, TopPatch) {
+            testSuite.initHistory();
+
+            var Auth = TopPatch.Auth,
+                test3Called = false,
+                test4Called = false,
+                test5Called = false,
+                AuthRouter = Router.extend({
+                    test3: function () {
+                        test3Called = true;
+                    }
+                }),
+                router = new AuthRouter();
+
+            Auth.signedIn = true;
+            Backbone.history.start({pushState: false});
+
+
+            // Route argument is string, name and callback are undefined
+            ok(router.authRoute('test1'), 'Attempt to define authRoute with route argument as string');
+            strictEqual(Backbone.history.handlers[0].route.source, '^test1$', 'Test1 route registered');
+            ok(router.navigate('test1', {trigger:true, replace:true}), 'navigate to "test1"');
+
+            // Route argument is RegExp, name and callback are undefined
+            ok(router.authRoute(/^test2$/), 'Attempt to define authRoute with route argument as RegExp');
+            strictEqual(Backbone.history.handlers[0].route.source, '^test2$', 'Test2 route registered');
+            ok(router.navigate('test2', {trigger:true, replace:true}), 'navigate to "test2"');
+
+            // Name is string, callback undefined
+            ok(router.authRoute('test3', 'test3'), 'Attempt to define authRoute with name argument as string');
+            ok(router.navigate('test3', {trigger:true, replace:true}), 'navigate to "test3"');
+            ok(test3Called, 'test3 method found by name, and called');
+
+            // Name is function, callback undefined
+            ok(router.authRoute('test4', function () {
+                test4Called = true;
+            }), 'Attempt to define authRoute with name argument as function');
+            ok(router.navigate('test4', {trigger:true, replace:true}), 'navigate to "test4"');
+            ok(test4Called, 'test4 method found by name, and called');
+
+            // All arguments set
+            ok(router.authRoute('test5', 'test5', function () {
+                test5Called = true;
+            }), 'Attempt to define authRoute with name argument as function');
+            ok(router.navigate('test5', {trigger:true, replace:true}), 'navigate to "test4"');
+            ok(test5Called, 'test5 method found by name, and called');
+
+            Auth.signedIn = false;
+            start();
+        });
+    });
+
+    asyncTest('authRoute', function ()  {
+        var testSuite = this;
+        require(['core/js/router', 'core/js/TopPatch/Auth'], function(Router, TopPatch) {
+            testSuite.initHistory();
+
+            var Auth = TopPatch.Auth,
+                AuthRouter = Router.extend({
+                    routes: {
+                        'login': $.noop
+                    }
+                }),
+                router = new AuthRouter();
+            ok(router.authRoute('restricted(/*subroute)', $.noop), 'Create "restricted" authRoute');
+
+            Backbone.history.start({pushState: false});
+
+            // Simulate user NOT signed in
+            Auth.signedIn = false;
+            ok(router.navigate('restricted/area', {trigger:true, replace:true}), 'navigate to "restricted" while NOT signed in');
+            strictEqual(Backbone.history.fragment, 'login', 'Access denied. Redirected to login');
+            strictEqual(router.attemptedRoute, 'restricted/area');
+
+            // Simulate user signed in
+            Auth.signedIn = true;
+            ok(router.navigate('restricted', {trigger:true, replace:true}), 'navigate to "restricted" while signed in');
+            strictEqual(Backbone.history.fragment, 'restricted', 'Access granted to restricted route');
+            strictEqual(router.attemptedRoute, null);
+
+            // Test clean up
+            Auth.signedIn = false;
+            start();
+        });
+    });
 });
