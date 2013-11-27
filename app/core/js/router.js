c48b1bdf352e1e1990150b324efcf4247ae5a6b4
Luis Aleman <Luis@toppatch.com>
2013-11-27 16:21:44 -0500

    Create a new authRoute method

--- app/core/js/router.js
+++ app/core/js/router.js
@@ -6,9 +6,10 @@
  * @extends Backbone.Router
  */
 define(
-    ['backbone'],
-    function () {
+    ['core/js/TopPatch/Auth', 'backbone'],
+    function (TopPatch) {
         'use strict';
+        var Auth = TopPatch.Auth;
         return Backbone.Router.extend({
             /**
              * Create an instance of Router.
@@ -49,7 +50,44 @@ define(
                 this.lastFragment = this.currentFragment;
                 this.currentFragment = Backbone.history.getFragment();
                 return this;
-            }
+            },
+
+            /**
+             * Manually create an authorized route for the router.
+             *
+             * It is mostly equivalent to the route method, only it checks if the user is signed in before continuing
+             *
+             * See: [Backbone.Router - route](http://backbonejs.org/#Router-route)
+             * @method authRoute
+             * @param route {string|RegExp} Routing string, or regular expression
+             * @param name {string|function} If string, will be triggered as "route:name". If function, will be used as callback.
+             * @param [callback] {function} If omitted, and name is a string, router[name] will be used instead
+             * @chainable
+             */
+            authRoute: function (route, name, callback) {
+                if (!_.isRegExp(route)) { route = this._routeToRegExp(route); }
+                if (_.isFunction(name)) {
+                    callback = name;
+                    name = '';
+                }
+                if (!callback) { callback = this[name]; }
+                var router = this;
+                Backbone.history.route(route, function(fragment) {
+                    var args = router._extractParameters(route, fragment);
+                    if (Auth.signedIn === true) {
+                        if (callback) {
+                            callback.apply(router, args);
+                        }
+                        router.trigger.apply(router, ['route:' + name].concat(args));
+                        router.trigger('route', name, args);
+                        Backbone.history.trigger('route', router, name, args);
+                    } else {
+                        router.attemptedRoute = fragment;
+                        router.navigate('login', {trigger:true, replace: true});
+                    }
+                });
+                return this;
+            },
         });
     }
 );
