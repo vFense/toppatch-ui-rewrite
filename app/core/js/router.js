d6022b5a14d7a59c238f29f7b997446fc9622a7c
Luis Aleman <Luis@toppatch.com>
2013-11-29 15:25:29 -0500

    Attempt to simplify authRoute

--- app/core/js/router.js
+++ app/core/js/router.js
@@ -64,26 +64,20 @@ define(
              * @chainable
              */
             authRoute: function (route, name, callback) {
-                if (!_.isRegExp(route)) { route = this._routeToRegExp(route); }
                 if (_.isFunction(name)) {
                     callback = name;
                     name = '';
                 }
                 if (!callback) { callback = this[name]; }
-                var router = this;
-                Backbone.history.route(route, function(fragment) {
-                    var args = router._extractParameters(route, fragment);
-                    if (Auth.signedIn === true) {
-                        router.attemptedRoute = null;
+                Backbone.Router.prototype.route.call(this, route, name, function () {
+                    if(Auth.signedIn === true) {
+                        this.attemptedRoute = null;
                         if (callback) {
-                            callback.apply(router, args);
+                            callback.apply(this, arguments);
                         }
-                        router.trigger.apply(router, ['route:' + name].concat(args));
-                        router.trigger('route', name, args);
-                        Backbone.history.trigger('route', router, name, args);
                     } else {
-                        router.attemptedRoute = fragment;
-                        router.navigate('login', {trigger:true, replace: true});
+                        this.attemptedRoute = Backbone.history.getFragment();
+                        this.navigate('login', {trigger:true, replace: true});
                     }
                 });
                 return this;
