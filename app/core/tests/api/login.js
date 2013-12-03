d075bda1ff485e83e0ded65b5bf1b4b86d4560b6
Luis Aleman <Luis@toppatch.com>
2013-12-03 17:37:03 -0500

    Update mock login api

--- app/core/tests/api/login.js
+++ app/core/tests/api/login.js
@@ -2,22 +2,37 @@ define(
     ['core/tests/api/mockApi'],
     function (mockApi) {
         'use strict';
+        function authenticate (user, pass) {
+            return user === 'test' && pass === 'test';
+        };
         return mockApi({
             url:  '/login',
             type: 'POST',
             response: function (settings) {
-                if (settings.data.name === 'test' && settings.data.password === 'test') {
-                    this.status = 200;
-                    this.statusText = 'OK';
-                    this.responseText = JSON.stringify({
-                        message: 'valid'
-                    });
+                var username = settings.data.username,
+                    password = settings.data.password;
+                if (username && password) {
+                    var authenticated = authenticate(username, password);
+
+                    if (authenticated) {
+                        this.status = 200;
+                        this.statusText = 'OK';
+                        this.responseText = JSON.stringify({
+                            user: {}
+                        });
+                    } else {
+                        this.status = 401;
+                        this.statusText = 'Unauthorized';
+                        this.responseText = JSON.stringify({
+                            error: 'Unauthorized'
+                        });
+                    }
                 } else {
-                    this.status = 401;
-                    this.statusText = 'Unauthorized';
-                    this.responseText = JSON.stringify({
-                        message: 'invalid'
-                    });
+                    var result = {};
+                    if (!username) { result.username = ['Username is required']; }
+                    if (!password) { result.username = ['Password is required']; }
+                    this.status = 400;
+                    this.responseText = JSON.stringify(result);
                 }
             }
         });
