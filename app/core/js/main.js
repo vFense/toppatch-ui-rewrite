ec5b32c59dc9f6a28a9f2e4f24a19e761210633c
Luis Aleman <Luis@toppatch.com>
2013-12-02 19:52:28 -0500

    Update core main to wait for plugin routers to load  before starting history

--- app/core/js/main.js
+++ app/core/js/main.js
@@ -1,8 +1,29 @@
 define(
-    ['core/js/TopPatch/TopPatch'],
-    function (TopPatch) {
+    ['core/js/TopPatch/TopPatch', 'require'],
+    function (TopPatch, require) {
         'use strict';
+        var plugins = {
+            RVault: true
+        };
+
+        TopPatch.loading = true;
         TopPatch.router = new TopPatch.Router();
-        Backbone.history.start();
+        TopPatch.modules = {};
+
+        if (plugins.RVault) {
+            plugins.RVault = $.Deferred();
+            require(
+                ['rvault/js/main'],
+                plugins.RVault.resolve,
+                function (error) {
+                    plugins.RVault.reject();
+                    throw error;
+                }
+            );
+        }
+
+        $.when.apply(null, _.values(plugins)).always(function () {
+            Backbone.history.start();
+        });
     }
 );
