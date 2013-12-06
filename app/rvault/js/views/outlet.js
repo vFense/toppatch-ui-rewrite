64ba1a2ae964deb32242f485eb971788db70a05f
Luis Aleman <Luis@toppatch.com>
2013-12-06 16:42:49 -0500

    Make first version of rvault outlet view

---
+++ app/rvault/js/views/outlet.js
@@ -0,0 +1,16 @@
+define(
+    [
+        'core/js/views/outlet',
+        'rvault/template/outlet',
+        'rvault/template/_header',
+        'rvault/template/_footer',
+        'bootstrap.collapse',
+        'bootstrap.dropdown'
+    ],
+    function (Outlet, template) {
+        'use strict';
+        return Outlet.extend({
+            template: template
+        });
+    }
+);
