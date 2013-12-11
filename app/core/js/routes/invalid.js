27d0a1f30196ab9bbd63ba8818e767e5a49f4687
Luis Aleman <Luis@toppatch.com>
2013-12-11 14:33:50 -0500

    Remove console.log

--- app/core/js/routes/invalid.js
+++ app/core/js/routes/invalid.js
@@ -3,7 +3,6 @@ define(
         'use strict';
         var View = require('core/js/views/invalid');
         return function () {
-            console.log(arguments);
             var view = new View();
             this.show(view.render());
             return this;
