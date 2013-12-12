27606cf0fa7c3956b0fcc2e463512ce4ce6863a4
sergiopolo <sergio@toppatch.com>
2013-12-12 14:44:03 -0500

    Added documentation to Region class

--- app/core/js/views/region.js
+++ app/core/js/views/region.js
@@ -1,25 +1,44 @@
-/**
- * A TemplateView that creates regions
- * @class Region
- * @extends TemplateView
- */
 define(
     ['core/js/views/template'],
     function (TemplateView) {
         'use strict';
         return TemplateView.extend({
+            /**
+             * A TemplateView that manages regions
+             * @class Region
+             * @extends TemplateView
+             * @constructor
+             * @returns {this}
+             */
             constructor: function () {
                 TemplateView.prototype.constructor.apply(this, arguments);
-                if (_.isUndefined(this.model)) {
-                    this.model = new Backbone.Model();
+                if (_.isUndefined(this.regions)) {
+                    this.regions = new Backbone.Model();
                 }
-                this.listenTo(this.model, 'change', this.render);
+                this.listenTo(this.regions, 'change', this.render);
+                return this;
             },
-            render: function (model) {
+            /**
+             * Renders the layout of regions
+             * @method render
+             * @chainable
+             */
+            render: function (regions) {
+                var that = this;
                 TemplateView.prototype.render.apply(this, arguments);
-                _.each(model.attributes, function (view, region) {
-                    this.$(region).html(view.$el || view);
+                _.each(regions.attributes, function (view, region) {
+                    that.$(region).html(view.$el || view);
                 });
+                return this;
+            },
+            /**
+             * Get the properties that will be passed to the template
+             * @method getData
+             * @override
+             * @returns {Object}
+             */
+            getData: function () {
+                return this.regions.toJSON();
             }
         });
     }
