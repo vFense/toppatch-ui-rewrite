1e51de8ce77acbdbaf08dcc57546d4dbf9d04e9d
Ravi Teja <ravi@toppatch.com>
2013-12-20 11:21:12 +0530

    placeholder routes added in rvault/router

--- app/rvault/js/routes/router.js
+++ app/rvault/js/routes/router.js
@@ -3,7 +3,11 @@ define(
         'use strict';
         return require('core/js/routes/_outletRouter').extend({
             authRoutes: {
-                'rvault(/)': require('./index')
+                'rvault(/)': require('./index'),
+                'rvault/agents(/)': require('./index'),
+                'rvault/applications(/)': require('./index'),
+                'rvault/schedules(/)': require('./index'),
+                'rvault/operations(/)': require('./index')
             },
 
             outlet: new (require('rvault/js/views/outlet'))()
