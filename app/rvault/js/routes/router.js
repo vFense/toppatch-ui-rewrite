648de3b03eadc6c27a6241561dfa072bf69ad135
Ravi Teja <ravi@toppatch.com>
2013-12-23 22:47:11 +0530

    required changes made in rvault/router

--- app/rvault/js/routes/router.js
+++ app/rvault/js/routes/router.js
@@ -4,10 +4,10 @@ define(
         return require('core/js/routes/_outletRouter').extend({
             authRoutes: {
                 'rvault(/)': require('./index'),
-                'rvault/agents(/)': require('./index'),
-                'rvault/applications(/)': require('./index'),
-                'rvault/schedules(/)': require('./index'),
-                'rvault/operations(/)': require('./index')
+                'rvault/agents(/)': require('./agents/index'),
+                'rvault/applications(/)': require('./applications/index'),
+                'rvault/schedules(/)': require('./schedules/index'),
+                'rvault/operations(/)': require('./operations/index')
             },
 
             outlet: new (require('rvault/js/views/outlet'))()
