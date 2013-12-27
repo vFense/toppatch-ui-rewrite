be8197de45634e58d0845571d3174fb471639daa
Luis Aleman <Luis@toppatch.com>
2013-12-26 19:59:21 -0500

    Major overhaul of views/region

--- app/core/js/views/region.js
+++ app/core/js/views/region.js
@@ -10,34 +10,129 @@ define(
              * @constructor
              * @returns {this}
              */
-            constructor: function () {
-                TemplateView.prototype.constructor.apply(this, arguments);
-                if (_.isUndefined(this.regions)) {
-                    this.regions = new Backbone.Model();
+            constructor: function (options) {
+                var regions = _.result(options, 'regions');
+                this.regions = {};
+                if (_.isObject(regions)) { this.set(regions); }
+                return TemplateView.prototype.constructor.apply(this, arguments);
+            },
+
+            get: function (attr) { return this.regions[attr]; },
+
+            /**
+             * Set a hash of regions (one or many) on the regions property
+             *
+             * Based on [Backbone.Model.set](http://backbonejs.org/#Model-set)
+             * @method set
+             * @param key {string|object}
+             * @param [value] {*}
+             * @param [options] {object}
+             * @chainable
+             */
+            set: function (key, value, options) {
+                var attrs, unset, render, current, changes;
+
+                // Parse arguments
+                if (_.isUndefined(key) || _.isNull(key)) { return this; }
+
+                if (_.isObject(key)) {
+                    attrs = key;
+                    options = value;
+                } else {
+                    (attrs = {})[key] = value;
                 }
-                this.listenTo(this.regions, 'change', this.render);
+
+                if (!_.isObject(options)) {
+                    options = {};
+                }
+
+                unset   = options.unset;
+                render  = options.render;
+                changes = [];
+                current = this.regions;
+
+                // Set regions
+                _.forIn(attrs, function (attr, key) {
+                    if (!_.isEqual(current[key], attr)) {
+                        changes.push(key);
+
+                        _.result(current[key], 'close');
+                    }
+
+                    if (unset) {
+                        delete current[key];
+                    } else {
+                        current[key] = attr;
+                    }
+                }, this);
+
+                // Render if options.render is true and there are changes
+                if (render) {
+                    if (this.isClosed !== false) {
+                        this.render();
+                    } else if (changes.length) {
+                        this._partialRender(_.pick(current, changes));
+                    }
+                }
+
                 return this;
             },
+
             /**
-             * Renders the layout of regions
+             * @method unset
+             * @param attr
+             * @param [options]
+             * @chainable
+             */
+            unset: function (attr, options) {
+                return this.set(attr, void 0, _.extend({}, options, { unset: true }));
+            },
+
+            /**
+             * @method clear
+             * @param [options]
+             * @chainable
+             */
+            clear: function(options) {
+                var attrs = _.mapValues(this.regions, function () { return void 0; });
+                return this.set(attrs, _.extend({}, options, { unset: true }));
+            },
+
+            /**
+             * @method _renderRegion
+             * @param content
+             * @param selector
+             * @chainable
+             * @private
+             */
+            _renderRegion: function (content, selector) {
+                if (content.isClosed !== false) {
+                    _.result(content, 'render');
+                }
+                var out = content.$el || content;
+                this.$(selector).html(out);
+                return this;
+            },
+
+            /**
+             * @method _partialRender
+             * @param changed
+             * @chainable
+             * @private
+             */
+            _partialRender: function (changed) {
+                _.forIn(changed, this._renderRegion, this);
+                return this;
+            },
+
+            /**
              * @method render
              * @chainable
              */
-            render: function (regions) {
+            render: function () {
                 TemplateView.prototype.render.apply(this, arguments);
-                _.each(regions.attributes, function (view, region) {
-                    this.$(region).html(view.$el || view);
-                }, this);
+                _.forIn(this.regions, this._renderRegion, this);
                 return this;
-            },
-            /**
-             * Get the properties that will be passed to the template
-             * @method getData
-             * @override
-             * @returns {Object}
-             */
-            getData: function () {
-                return this.regions.toJSON();
             }
         });
     }
