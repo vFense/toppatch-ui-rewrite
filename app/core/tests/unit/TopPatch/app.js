b9a17f8900b8b2ce1986ba0e74ba70759aac4a78
Luis Aleman <Luis@toppatch.com>
2013-12-27 13:17:08 -0500

    Test app.show

--- app/core/tests/unit/TopPatch/app.js
+++ app/core/tests/unit/TopPatch/app.js
@@ -2,6 +2,40 @@ $(document).ready(function () {
     'use strict';
     module('TopPatch/app');
 
+    asyncTest('show', function () {
+        require(
+            ['core/js/TopPatch/app'],
+            function (App) {
+                var element = $('<div>').attr('id', App.prototype.el.slice(1)).appendTo($('body')),
+                    app = new App(),
+                    simView1 = {$el: $('<div>').html('view1')},
+                    simView2 = {$el: $('<div>').html('view2')},
+                    called = 0;
+
+                app.closeChildViews = function () {
+                    called += 1;
+                    return this;
+                };
+
+                app.show(simView1);
+                strictEqual(app.$el[0].innerHTML, simView1.$el[0].outerHTML, 'html is correct');
+                strictEqual(called, 1, 'Called closeChildViews');
+
+                app.show(simView2);
+                strictEqual(app.$el[0].innerHTML, simView2.$el[0].outerHTML, 'html is correct');
+                strictEqual(called, 2, 'Called closeChildViews');
+
+                app.show(simView2);
+                strictEqual(app.$el[0].innerHTML, simView2.$el[0].outerHTML, 'html is correct');
+                strictEqual(called, 2, 'Did NOT call closeChildViews');
+
+                app.close();
+                element.remove();
+                start();
+            }
+        );
+    });
+
     asyncTest('setDocumentTitle', function () {
         require(
             ['core/js/TopPatch/app'],
